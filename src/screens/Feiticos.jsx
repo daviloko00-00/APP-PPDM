f4a3ed8d88c9f0e50cc027f83c062ce8dcaefa96
RAÍSSA PEREIRA DA SILVA BEZERRA <raissa.bezerra2@senaisp.edu.br>
2026-09-21 13:07:08 -0300

    :sparkles: feat: instalando pacotes de fontes e criando a api.js

--- src/screens/Feiticos.jsx
+++ src/screens/Feiticos.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PlayfairDisplay_400Regular } from '@expo-google-fonts/playfair-display';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Feitiços</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    fontFamily: 'PlayfairDisplay_400Regular',
+},
 });
